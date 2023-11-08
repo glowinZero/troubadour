394f1ad2648da8bd450a5344053d4503c2e73a44
Bruno Carneiro <b.h.r.carneiro@gmail.com>
2023-11-08 16:38:27 +0000

    create users + dropdown

--- troubadour-project/src/Components/Navbar/index.jsx
+++ troubadour-project/src/Components/Navbar/index.jsx
@@ -1,10 +1,32 @@
 import { Link } from "react-router-dom"
 import '../../App.css'
+import axios from "axios"
+import { useState } from "react"
+
+const userApi = "http://localhost:5178"
 
 function Navbar () {
+    const[username, setUsername] = useState("");
+    const[password, setPassword] = useState("");
+    
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const requestBody = {username, password};
+        axios.post(`${userApi}/users`, requestBody).then(()=>{
+            setUsername("")
+            setPassword("")
+        }
+        ).catch(error=>{console.log(error)})
+    }
     return (
         <nav id="navbar">
             <Link to="/playlists">Playlist</Link>
+            <form onSubmit={handleSubmit}>
+                <h4>Signup</h4>
+                <label><input type="text" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)}/></label>
+                <label><input type="password" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}/></label>
+                <button type="submit">Submit</button>
+            </form>
         </nav>
     )
 }
--- troubadour-project/src/Pages/HomePage/index.jsx
+++ troubadour-project/src/Pages/HomePage/index.jsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom"
+
 function HomePage(){
+    const navigate = useNavigate()
+    const handleSubmit = ()=>{
+        navigate("/playlists") 
+    } 
     return (
         <div>
-            Olá
+            <h1>Troubadour</h1>
+            <p>How are you feeling today?</p>
+            <select name="How are you feeling today?">
+                <option value={0}>Neutral</option>
+                <option value={1}>Happy</option>
+                <option value={2}>Sad</option>
+            </select>
+            <button onClick={()=>handleSubmit()}>Submit</button>
         </div>
     )
 }
