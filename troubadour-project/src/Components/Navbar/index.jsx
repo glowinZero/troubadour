50b42191b5aedf33d28164af808f828b4650866f
Bruno Carneiro <b.h.r.carneiro@gmail.com>
2023-11-08 12:02:18 +0000

    react-router + Navbar

---
+++ troubadour-project/src/Components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+function Navbar () {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+        </div>
+    )
+}
+
+export default Navbar
