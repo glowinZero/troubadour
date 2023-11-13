e1fc1c727055ef0ceb173dc484592183faa6281c
Jakob Bodendieck <bodendieck.jakob@gmail.com>
2023-11-13 15:57:13 +0000

    Spotify bug fix, server fixed

--- troubadour-project/src/Components/Navbar/index.jsx
+++ troubadour-project/src/Components/Navbar/index.jsx
@@ -4,9 +4,7 @@ import '../../App.css'
 import axios from "axios"
 import { useEffect, useState } from "react"
 import logo from "../../../public/noun-banjo-5393194 (1).png"
-
 const userApi = "http://localhost:5178"
-
 function Navbar () {
     const [users, setUsers] = useState([]);
     const [id, setId] = useState();
@@ -17,20 +15,22 @@ function Navbar () {
     const [loggedin, setLoggedin] = useState(false)
     const [popupOpen, setPopupOpen] = useState(false);
     const navigate = useNavigate();
-    
     useEffect(() => {
-        axios.get(`${userApi}/users`).then((response)=>{
-            setUsers(response.data) 
-        }).catch(error=>{console.log(error)})
-
+        if (!users.length) {
+            axios.get(`${userApi}/users`)
+                .then((response) => {
+                    setUsers(response.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
         const storedUsername = localStorage.getItem("username");
-
         if (!loggedin && storedUsername !== null) {
             setUsername(storedUsername);
             const userFound = users.some(user => user.username === storedUsername);
             setLoggedin(userFound);
         }
-        
         if (!loggedin) {
             updateUsernameFromStorage();
             if (window.location.pathname === "/" && performance.navigation.type !== 1) {
@@ -38,14 +38,12 @@ function Navbar () {
             }
         }
     }, [loggedin, navigate, users]);
-
     const updateUsernameFromStorage = () => {
         const storedUsername = window.localStorage.getItem("username");
         if (storedUsername && !username) {
             setUsername(storedUsername);
         }
     };
-    
     const handleSubmit = (e, close) =>{
         e.preventDefault()
         if (!inputUsername && !password) {
@@ -59,11 +57,9 @@ function Navbar () {
                     localStorage.removeItem("username");
                     close();
                   });
-              } 
+              }
           }
         localStorage.setItem("username", inputUsername);
-
-        
         const requestBody = {inputUsername, password};
         formType === "signup" ? axios.post(`${userApi}/users`, requestBody).then(()=>{
             setLoggedin(true)
@@ -72,23 +68,20 @@ function Navbar () {
         }).catch(error=>{console.log(error)}) : axios.get(`${userApi}/users`).then((response)=>{
             setUsers(response.data)
             const userID = users.filter((user)=>{return (user.username === inputUsername)})
-            setId(userID[0].id); 
+            setId(userID[0].id);
             if (users.some(user => user.username === inputUsername && user.password === password )) {
                 setLoggedin(true)
                 close();
-                navigate(`/mood/${userID[0].id}`) 
+                navigate(`/mood/${userID[0].id}`)
             } else {
                 setInputUsername("");
                 setPassword("")
                 alert("User or password is incorrect or missing");
             }}).catch(error=>{console.log(error)})
     }
-
     const toggleForm = () =>{
         setFormType(formType === "login" ? "signup" : "login");
     }
-
-
     const logOut = () => {
         setLoggedin(false);
         setUsername("");
@@ -97,30 +90,26 @@ function Navbar () {
         localStorage.removeItem("username");
         navigate("/");
     };
-
     const openPopup = () => {
         setPopupOpen(true);
     };
-    
     const closePopup = () => {
         setPopupOpen(false);
     };
-
     const editUser = () =>{
         console.log("Navigating to edit page with id:", id);
         navigate(`/edit/${id}`)
         setPopupOpen(false);
         closePopup();
     }
-
     return (
         <nav id="navbar">
             <img id="logo-bar" src={logo}/>
             {loggedin && <div id="menu">
                 <Link id="link-menu" to="/playlists/:userId/:mood"> Playlists</Link>
             </div>}
-            <Popup trigger={<button id="popup" onClick={openPopup}>{!username && !loggedin ? <p>Login</p> : <p>{username}</p>}</button>} 
-            modal 
+            <Popup trigger={<button id="popup" onClick={openPopup}>{!username && !loggedin ? <p>Login</p> : <p>{username}</p>}</button>}
+            modal
             nested
             open={popupOpen}
             onClose={closePopup}>
@@ -143,12 +132,10 @@ function Navbar () {
                             <button type="button" onClick={()=>{editUser()}}>Edit account</button>
                         </div>
                         )}
-                </form> 
+                </form>
                 )}
             </Popup>
-
         </nav>
     )
 }
-
 export default Navbar
