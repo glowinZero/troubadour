99da31aa655b3c91d35a1656ea816a1187046b56
Bruno Carneiro <b.h.r.carneiro@gmail.com>
2023-11-13 16:08:19 +0000

    updated navbar

--- troubadour-project/src/main.jsx
+++ troubadour-project/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './App.css'
+import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import SpotifyPlayer from "react-spotify-web-playback"
 
