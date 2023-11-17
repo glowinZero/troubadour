0a1b5f80d537201e75bccdfa1404206a756f62f7
Bruno Carneiro <b.h.r.carneiro@gmail.com>
2023-11-17 08:27:48 +0000

    styling share button

--- troubadour-project/src/Pages/Playlists/index.jsx
+++ troubadour-project/src/Pages/Playlists/index.jsx
@@ -147,7 +147,7 @@ function Playlists() {
                   style={{ borderRadius: "12px", marginRight: "200px" }}
                   src={`https://open.spotify.com/embed/playlist/${playlistLink}`}
                   width="100%"
-                  height="380"
+                  height="360"
                   frameBorder="0"
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                   allowFullScreen
