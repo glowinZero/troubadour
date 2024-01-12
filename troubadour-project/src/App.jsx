a0d9e63f9535cf4ddd3623ad5b33b8a3f6a409b6
Jakob Bodendieck <bodendieck.jakob@gmail.com>
2024-01-12 12:05:38 +0000

    spotify playlists are showing again

--- troubadour-project/src/App.jsx
+++ troubadour-project/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
-          <Route path="/mood/:userId" element={<Mood/>}/>
+          <Route path="/mood/" element={<Mood/>}/>
           <Route path="/playlists/:userId/:mood" element={<Playlists/>}/>
           <Route path="/edit/:userId" element={<EditUser/>}/>
           <Route path='/history/:userId' element={<PlaylistHistory/>}/>
